20398f1481bdfc519a5230cca2f2cf2f71e4dafa
Alex Varga <alexvarga09072001@gmail.com>
2022-12-29 14:25:39 +0200

    Adding dist

---
+++ dist/assets/Plant-651524d8.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index-0dab1220.js";const t={};function o(r,a){return c(),n("h1",null,"Plant")}const _=e(t,[["render",o]]);export{_ as default};
---
+++ dist/assets/Tables-e5d6e775.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index-0dab1220.js";const o={};function r(s,t){return c(),n("h1",null,"Tables")}const _=e(o,[["render",r]]);export{_ as default};
